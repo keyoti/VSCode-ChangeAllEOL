6812ae52f7f9778f76950d664083e2ea611d4a97
Daryl Marco <darmarco@gmail.com>
2020-06-04 14:08:11 -0700

    Changed from using ignore file to creating white & black lists from option prompt.

--- src/extension.ts
+++ src/extension.ts
@@ -13,12 +13,15 @@ export function activate(context: vscode.ExtensionContext) {
 	// Runs 'Change All End Of Line Sequence' on all files of specified type.
 	vscode.commands.registerCommand('keyoti/changealleol', async function () {
 
-		async function convertLineEndingsInFilesInFolder(folder: vscode.Uri, fileTypeArray: Array<string>, newEnding: string, ignoreArray: Array<string>): Promise<{ count: number }> {
+		async function convertLineEndingsInFilesInFolder(folder: vscode.Uri, fileTypeArray: Array<string>, newEnding: string, blackList: Array<string>, whiteList: Array<string>): Promise<{ count: number }> {
 			let count = 0;
 
 			for (const [name, type] of await vscode.workspace.fs.readDirectory(folder)) {
 
-				if (type === vscode.FileType.File && !ignoreArray.includes(name) && fileTypeArray.filter( (el)=>{return name.endsWith(el);} ).length>0){
+				if (type === vscode.FileType.File
+					&& !blackList.includes(name)
+					&& (!whiteList.length || whiteList.includes(name))
+					&& fileTypeArray.filter( (el)=>{return name.endsWith(el);} ).length>0){
 					const filePath = posix.join(folder.path, name);
 
 					var doc = await vscode.workspace.openTextDocument(filePath);
@@ -39,8 +42,12 @@ export function activate(context: vscode.ExtensionContext) {
 					}
 				}
 
-				if (type === vscode.FileType.Directory && !ignoreArray.includes(name) && !name.startsWith(".")){
-					count += (await convertLineEndingsInFilesInFolder(vscode.Uri.file(posix.join(folder.path, name)), fileTypeArray, newEnding, ignoreArray)).count;
+				if (type === vscode.FileType.Directory
+					&& !blackList.includes(name)
+					&& (!whiteList.length || whiteList.includes(name))
+					&& !name.startsWith(".")){
+					let newWhiteList: Array<string> =  whiteList.includes(name) ? [] : [...whiteList]
+					count += (await convertLineEndingsInFilesInFolder(vscode.Uri.file(posix.join(folder.path, name)), fileTypeArray, newEnding, blackList, newWhiteList)).count;
 				}
 			}
 			return { count };
@@ -48,28 +55,29 @@ export function activate(context: vscode.ExtensionContext) {
 
 		let options: vscode.InputBoxOptions = {prompt: "File types to convert", placeHolder: ".cs, .txt", ignoreFocusOut: true};
 		let fileTypes = await vscode.window.showInputBox(options);
-		let optionsIgnore: vscode.InputBoxOptions = {prompt: "Name of ignore file (optional)", placeHolder: "Use a file in the project's root directory with a line separated list of directories & files (no wildcards) to ignore", ignoreFocusOut: true};
-		let ignoreFile = await vscode.window.showInputBox(optionsIgnore);
+		let blackWhiteOptions: vscode.InputBoxOptions = {prompt: "Add file/dir names for whitelist or blacklist (prefix with a !) (optional)", placeHolder: "!node_modules, src (no wildcards)", ignoreFocusOut: true};
+		let blackWhite = (await vscode.window.showInputBox(blackWhiteOptions))!.split(/[ ,]+/);
 		fileTypes = fileTypes!.replace(' ', '');
 		let fileTypeArray: Array<string> = [];
-
-		let newEnding = await vscode.window.showQuickPick(["LF", "CRLF"]);
-
-		const ignoreList = await vscode.workspace.openTextDocument(posix.join(vscode.workspace.workspaceFolders![0].uri.path, ignoreFile));
-		const ignoreArray: Array<string>  = [];
-		await vscode.window.showTextDocument(ignoreList);
-		if (vscode.window.activeTextEditor !== null) {
-			for (let i = 0; i < vscode.window.activeTextEditor!.document.lineCount; i++) {
-					ignoreArray.push(vscode.window.activeTextEditor!.document.lineAt(i).text);
+		let whiteList: Array<string> = [];
+		let blackList: Array<string> = [];
+		for (let x of blackWhite) {
+			if (x[0] === '!') {
+				blackList.push(x.substring(1))
+			} else {
+				whiteList.push(x)
 			}
 		}
+		console.log(blackWhite, whiteList, blackList)
+
+		let newEnding = await vscode.window.showQuickPick(["LF", "CRLF"]);
 
 		if(fileTypes!==null && newEnding!=null){
 			fileTypeArray = fileTypes!.split(',');
 
 			if(vscode.workspace.workspaceFolders!==null && vscode.workspace.workspaceFolders!.length>0){
 				const folderUri = vscode.workspace.workspaceFolders![0].uri;
-				const info = await convertLineEndingsInFilesInFolder(folderUri, fileTypeArray, newEnding, ignoreArray);
+				const info = await convertLineEndingsInFilesInFolder(folderUri, fileTypeArray, newEnding, blackList, whiteList);
 				vscode.window.showInformationMessage(info.count+" files converted");
 
 			}
